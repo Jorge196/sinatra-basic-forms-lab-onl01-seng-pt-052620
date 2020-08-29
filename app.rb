da07a4b7027d73d09943981aef568043ec56aac6
Jorge Ramirez <joramirez96@gmail.com>
2020-08-29 16:53:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,6 +8,13 @@ class App < Sinatra::Base
 
   get '/new' do
     erb :create_puppy
+  end
+
+  post '/new' do
+    @name = params[:name]
+    @breed = params[:breed]
+    @age = params[:age]
+    erb :display_puppy
   end 
 
 end
