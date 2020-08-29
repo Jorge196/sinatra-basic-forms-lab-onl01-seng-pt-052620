1a665e7a7ee2f6b9cbc9219ce15118fe47f44bfa
Jorge Ramirez <joramirez96@gmail.com>
2020-08-29 16:46:41 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -2,9 +2,9 @@
 class Puppy
   attr_accessor :name, :breed, :age
 
-  def initialize(name, age, breed)
+  def initialize(name, breed, age)
     @name = name
-    @age = age
     @breed = breed
+    @age = age
   end
 end
